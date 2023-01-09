8e87a098ccda70e7b7be7e81877e2dcf5caf650d
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-01-09 18:54:27 +0500

    add option to use in folder

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const fs = require("fs");
+const path = require("path");
 
 const replaceThis = "write";
 const replaceWith = "read";
-const preview = true;
+const preview = false;
+const folder = __dirname;
 
 try {
-  const data = fs.readdir("data", (err, data) => {
+  const data = fs.readdir(folder, (err, data) => {
     for (let index = 0; index < data.length; index++) {
       const item = data[index];
-
-      let newFile = "data/" + item.replaceAll(replaceThis, replaceWith);
+      let oldFile = path.join(folder, item);
+      let newFile = path.join(
+        folder,
+        item.replaceAll(replaceThis, replaceWith)
+      );
       if (!preview) {
-        fs.rename("data/" + item, newFile, () => {
+        fs.rename(oldFile, newFile, () => {
           console.log("Rename Success");
         });
       } else {
