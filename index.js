60ebb1bcd27c9738b33b76fa5e52d8e566e227e1
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2023-01-09 18:25:10 +0500

    add preview option

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const fs = require("fs");
 
-const replaceThis = "read";
-const replaceWith = "write";
+const replaceThis = "write";
+const replaceWith = "read";
+const preview = true;
 
 try {
   const data = fs.readdir("data", (err, data) => {
-    console.log(data);
     for (let index = 0; index < data.length; index++) {
       const item = data[index];
 
       let newFile = "data/" + item.replaceAll(replaceThis, replaceWith);
-      fs.rename("data/" + item, newFile, () => {
-        console.log("Rename Success");
-      });
+      if (!preview) {
+        fs.rename("data/" + item, newFile, () => {
+          console.log("Rename Success");
+        });
+      } else {
+        if ("data/" + item !== newFile)
+          console.log("data/" + item + " will be renamed to " + newFile);
+      }
     }
   });
   console.log(data);
